3529d3be35b07706c18bc1806e8975a81b20a030
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-06-03 02:27:38 -0300

    ADD a-javascript: primeras practicas

--- a-javascript/javascript.js
+++ a-javascript/javascript.js
@@ -1,1 +1,59 @@
-alert('funcionando')
+/*Pruebas basicas */
+/* alert('funcionando');
+
+var nombre = "Nicolas Korzusehec";
+var altura = 175;
+var concatenacion = nombre + " " + altura;
+
+console.log(concatenacion)
+
+
+
+if(altura>=150){
+div.innerHTML += `
+<p>No sos bajito, medis ${altura} cm</p>
+`;
+}else{
+div.innerHTML = `
+<p>Que enano</p>
+`;
+};
+
+for(var i = 200; i < 250; i+=4){
+div.innerHTML += `
+<p>${i}</p>
+`;
+console.log(i);
+}; 
+*/
+
+/*Ejemplo de ciclo y alert*/
+/* function MuestraMiNombre(nombre, altura){
+    var misDatos = `
+    <h1>primer texto</h1>
+    <h2>segundo texto</h2>
+    <h3>Me llamo ${nombre} </h3>
+    <h3>Mido ${altura} cm</h3>
+    `;
+    return misDatos;
+};
+
+function imprimir(){
+    var div = document.getElementById('prueba');
+    div.innerHTML = MuestraMiNombre('pinpong', 18);
+};
+
+imprimir();
+
+var nombres = ['Juan', 'German', 'Manuel'];
+
+function todosLosNombres( arreglo){
+    for (let element of arreglo){
+        alert(element);
+
+    };
+};
+
+todosLosNombres(nombres); */
+
+
